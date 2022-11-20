c945268d85e3442b3996be403307ed71a3adecdc
ringsaturn <ringsaturn.me@gmail.com>
2022-11-20 21:04:10 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+use lazy_static::lazy_static;
+use pyo3::prelude::*;
+use tzf_rs::DefaultFinder;
+
+lazy_static! {
+    static ref FINDER: DefaultFinder = DefaultFinder::new();
+}
+
+#[pyfunction]
+pub fn get_tz(lng: f64, lat: f64) -> PyResult<String> {
+    Ok(FINDER.get_tz_name(lng, lat).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn tzfrspy(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_tz, m)?)?;
+    Ok(())
+}
